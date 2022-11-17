2aee0b6c700a8c66563603d112588767f5ccb847
Arturo López <lgzarturo@gmail.com>
2022-11-17 17:39:41 -0500

    Agregando otra prueba

--- next.config.js
+++ next.config.js
@@ -10,10 +10,6 @@ const nextConfig = {
             type: 'host',
             value: 'visitapormexico.com',
           },
-          {
-            type: 'host',
-            value: 'www.visitapormexico.com',
-          },
         ],
         destination: '/welcome/:path*',
         permanent: true,
@@ -25,12 +21,13 @@ const nextConfig = {
             type: 'host',
             value: 'joobslot.com',
           },
-          {
-            type: 'host',
-            value: 'www.joobslot.com',
-          },
         ],
         destination: '/jobs/:path*',
+        permanent: true,
+      },
+      {
+        source: '/contact',
+        destination: 'https://twitter.com/lgzarturo',
         permanent: true,
       },
     ]
